006d7b34f51a612b89a1ab1e8b0fb850a4999076
prtsie <yonhsup@mail.ru>
2025-09-20 19:17:12 +0300

    Удалил проект с тг-ботом, создал проект для вебхуков

---
+++ PRManager.Web/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRManager.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
